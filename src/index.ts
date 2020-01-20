494969ad51dfce4170e57aef6253d0281bc416ff
Bao Zhen <baozhen@xiaoyezi.com>
2020-01-20 23:40:21 +0800

    Add Telegraf
    Change-Id: Id2e2de3277c816496de8a86c97c70d43353c35fe

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,11 @@ import { isValidPrivate } from 'eosjs-ecc';
 import * as chalk from 'chalk';
 import * as figlet from 'figlet';
 import getApiEndpoints from 'eos-endpoint';
+import Telegraf from 'telegraf';
 
-console.log(isValidPrivate);
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
