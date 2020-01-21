e2fc72499d0806bd626e45956ac216c934ffcdb8
Bao Zhen <baozhen@xiaoyezi.com>
2020-01-21 09:31:32 +0800

    move folder
    Change-Id: I0b5414fb2047379c3022cdd2d63c027e2d7cdeb5

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import * as yargs from 'yargs';
-import {EosClient, ITokenDesc} from './EosClient';
-import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
-import { isValidPrivate } from 'eosjs-ecc';
-import * as chalk from 'chalk';
-import * as figlet from 'figlet';
-import getApiEndpoints from 'eos-endpoint';
+// import * as yargs from 'yargs';
+// import {EosClient, ITokenDesc} from './eos/EosClient';
+// import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
+// import { isValidPrivate } from 'eosjs-ecc';
+// import * as chalk from 'chalk';
+// import * as figlet from 'figlet';
+// import getApiEndpoints from 'eos-endpoint';
 import Telegraf from 'telegraf';
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
@@ -14,3 +14,4 @@ bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
+console.log("telegraf started");
